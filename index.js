e90256917a1b150a4602474f0af177028ff349bb
rober <zhuhongtao@silview.com>
2018-07-17 19:59:32 +0800

    测试路由功能

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-var http = require('http');
+var server = require("./server");
+var router = require("./router");
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
-});
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
-console.log('测试结果');
-console.log('master分支延申测试');
+server.start(router.route);
