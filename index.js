1b9d735db098f1c521c1195f06057f0566781da5
rober <zhuhongtao@silview.com>
2018-07-17 19:06:12 +0800

    增加master分支延申测试

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
-console.log('测试结果')
+console.log('测试结果');
+console.log('master分支延申测试');
