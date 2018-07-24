755f4586713e8a40f0fd59b2cc5a0590f6ff5af2
rober <zhuhongtao@silview.com>
2018-07-24 18:03:04 +0800

    新建Test Branch 2分支

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var server = require("./server");
 var router = require("./router");
+var fs = require('fs');
 
 server.start(router.route);
+
+function printHello() {
+    console.log("Hello Word");
+}
+
+var t = setTimeout(printHello, 1000);
+
+clearTimeout(t);
+
+console.trace();
