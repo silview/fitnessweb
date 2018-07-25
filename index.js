4fc0002f0aa21a23d6b2bdf2fd0178c7b6323756
rober <zhuhongtao@silview.com>
2018-07-25 13:23:38 +0800

    增加路由功能，增加VS code同时前端后端调试配置

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 var server = require("./server");
 var router = require("./router");
-var fs = require('fs');
-
+ 
 server.start(router.route);
-
-function printHello() {
-    console.log("Hello Word");
-}
-
-var t = setTimeout(printHello, 1000);
-
-clearTimeout(t);
-
-console.trace();
